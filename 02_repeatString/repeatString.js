ffaa6df297f39fb525d98df4552e8fe9cc781db8
abdullah safaee <abdullahsafzah2005@gmail.com>
2023-09-18 12:22:33 +0300

    finish all exercises in one commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let string2 = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i<num; i++){
+        string2 = string2 + string
+    }
+    return string2
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	return n1+n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1-n2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i< nums.length; i++){
+    sum += nums[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let sum = 1
+  for (let i = 0; i < nums.length; i++){
+    sum *= nums[i]
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+	return base ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 0) return;
+  if (n < 2) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
